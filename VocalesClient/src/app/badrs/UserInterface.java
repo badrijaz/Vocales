be2e91f02e5072b848c5fac9c7016994703c23e0
itsbadr <me@badrs.app>
2021-07-20 19:04:09 +0500

    add GUI for interactivity

---
+++ VocalesClient/src/app/badrs/UserInterface.java
@@ -0,0 +1,25 @@
+package app.badrs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserInterface extends JFrame {
+
+    private JButton connectButton = new JButton("Connect");
+
+    public UserInterface(Thread serverThread) {
+        super("Vocales");
+
+        GridLayout gridLayout = new GridLayout(0, 2);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+
+        getContentPane().setLayout(gridLayout);
+        getContentPane().add(connectButton);
+    }
+
+}
