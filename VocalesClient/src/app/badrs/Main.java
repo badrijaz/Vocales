1e6f29621cb92a7bf2bea94c2c7c81fa44935b24
itsbadr <me@badrs.app>
2021-07-20 14:51:55 +0500

    establish a connection to the server

--- VocalesClient/src/app/badrs/Main.java
+++ VocalesClient/src/app/badrs/Main.java
@@ -1,8 +1,22 @@
 package app.badrs;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("hello world");
+    /* Socket and Packet */
+    private static String IP_ADDRESS = "192.168.10.4";
+    private static final int PORT = 55286;
+    private static InetSocketAddress address = new InetSocketAddress(IP_ADDRESS, PORT);
+    private static final int BUFFER_SIZE = 3428;
+    private static DatagramSocket serverSocket;
+    private static DatagramPacket datagramPacket;
+
+    public static void main(String[] args) throws Exception {
+	    /* Initialize and connect to Android */
+        serverSocket = new DatagramSocket(null);
+        serverSocket.connect(address);
     }
 }
