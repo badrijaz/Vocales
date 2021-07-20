945d36e6d74ce87d4c5fda56239d35e3a17b3e7f
itsbadr <me@badrs.app>
2021-07-20 15:17:34 +0500

    method to play audio to default mixer

--- VocalesClient/src/app/badrs/Util.java
+++ VocalesClient/src/app/badrs/Util.java
@@ -1,10 +1,19 @@
 package app.badrs;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Mixer;
+import javax.sound.sampled.*;
 import java.util.Scanner;
 
 public class Util {
+
+    /* Audio Configuration */
+    private static final int SAMPLE_RATE = 44100;
+    private static final short PCM_ENCODING = 16;
+    private static final short CHANNEL_IN = 1;
+
+    private static AudioFormat format;
+    private static DataLine.Info dataLineInfo;
+    private static SourceDataLine sourceDataLine;
+    private static FloatControl volume;
 
     private static final String PLAYBACK_MIXER = "Port CABLE Input";
 
@@ -18,6 +27,20 @@ public class Util {
 
         // Return the default playback device if not found
         return AudioSystem.getMixerInfo()[0];
+    }
+
+    public static void playAudioFromBytes(byte[] serverBytes) {
+        try {
+            format = new AudioFormat(SAMPLE_RATE, PCM_ENCODING, CHANNEL_IN, true, false);
+            dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+
+            sourceDataLine.open(format);
+            sourceDataLine.start();
+            sourceDataLine.write(serverBytes, 0, serverBytes.length);
+        } catch (LineUnavailableException lineUnavailableException) {
+            lineUnavailableException.printStackTrace();
+        }
     }
 
     public static void log(String stringToLog) {
