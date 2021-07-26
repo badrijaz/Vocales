59e3f01d3ca1a08f27e80f42b8f210a9b9346c2b
itsbadr <me@badrs.app>
2021-07-26 21:03:50 +0500

    show new activity on settings item click

---
+++ VocalesAndroid/app/src/main/java/app/badrs/vocales/MicrophoneSettingsActivity.java
@@ -0,0 +1,14 @@
+package app.badrs.vocales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MicrophoneSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_microphone_settings);
+    }
+}
