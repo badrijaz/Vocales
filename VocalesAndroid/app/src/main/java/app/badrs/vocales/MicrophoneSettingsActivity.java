277807c5b214aef8f646eddf9a8dc2b044418f16
itsbadr <me@badrs.app>
2021-07-28 13:04:45 +0500

    enable NoiseSuppressor/EchoCanceler on check events

--- VocalesAndroid/app/src/main/java/app/badrs/vocales/MicrophoneSettingsActivity.java
+++ VocalesAndroid/app/src/main/java/app/badrs/vocales/MicrophoneSettingsActivity.java
@@ -2,13 +2,38 @@ package app.badrs.vocales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.audiofx.AcousticEchoCanceler;
+import android.media.audiofx.NoiseSuppressor;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MicrophoneSettingsActivity extends AppCompatActivity {
+
+    /* Audio Configurations */
+    private final NoiseSuppressor noiseSuppressor
+            = NoiseSuppressor.create(MainActivity.audioRecorder.getAudioSessionId());
+    private final AcousticEchoCanceler acousticEchoCanceler
+            = AcousticEchoCanceler.create(MainActivity.audioRecorder.getAudioSessionId());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_microphone_settings);
+
+        initializeViews();
     }
+
+    public void initializeViews() {
+        /* CheckBox */
+        CheckBox noiseSuppressionCheckBox = findViewById(R.id.noiseSuppressionCheckBox);
+        CheckBox echoCancellationCheckBox = findViewById(R.id.echoCancellationCheckBox);
+
+        noiseSuppressionCheckBox.setOnCheckedChangeListener((buttonView, isChecked) ->
+                noiseSuppressor.setEnabled(isChecked));
+
+        echoCancellationCheckBox.setOnCheckedChangeListener((buttonView, isChecked) ->
+                acousticEchoCanceler.setEnabled(isChecked));
+    }
+
+
 }
