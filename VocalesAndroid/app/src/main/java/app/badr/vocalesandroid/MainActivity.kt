998823c62b89ac6bd925027cd9a072aed2ff45f8
Badr <badrkmc@gmail.com>
2023-02-24 00:04:06 +0500

    add structure for microphone enable on fab press

--- VocalesAndroid/app/src/main/java/app/badr/vocalesandroid/MainActivity.kt
+++ VocalesAndroid/app/src/main/java/app/badr/vocalesandroid/MainActivity.kt
@@ -1,5 +1,8 @@
 package app.badr.vocalesandroid
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +13,18 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import app.badr.vocalesandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var appBarConfiguration: AppBarConfiguration
   private lateinit var binding: ActivityMainBinding
+  private var requestCodeMicrophone = 100
 
+  @RequiresApi(Build.VERSION_CODES.M)
   override fun onCreate(savedInstanceState: Bundle?) {
     WindowCompat.setDecorFitsSystemWindows(window, false)
     super.onCreate(savedInstanceState)
@@ -31,10 +39,27 @@ class MainActivity : AppCompatActivity() {
     setupActionBarWithNavController(navController, appBarConfiguration)
 
     binding.fab.setOnClickListener { view ->
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
+      when (ContextCompat.checkSelfPermission(
+        this, Manifest.permission.RECORD_AUDIO
+      )) {
+        PackageManager.PERMISSION_GRANTED -> {
+          enableMicrophone()
+        }
+        PackageManager.PERMISSION_DENIED -> {
+          this.requestPermissions(
+            arrayOf(Manifest.permission.RECORD_AUDIO),
+            requestCodeMicrophone
+          )
+        }
+      }
+      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        .setAnchorView(R.id.fab)
+        .setAction("Action", null).show()
     }
+  }
+
+  private fun enableMicrophone() {
+    throw NotImplementedError("Yet to be implemented")
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -57,5 +82,26 @@ class MainActivity : AppCompatActivity() {
     val navController = findNavController(R.id.nav_host_fragment_content_main)
     return navController.navigateUp(appBarConfiguration)
             || super.onSupportNavigateUp()
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    when (requestCode) {
+      requestCodeMicrophone -> {
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          enableMicrophone()
+        } else {
+          Toast.makeText(
+            this,
+            "Enable your microphone to use this feature",
+            Toast.LENGTH_LONG).show()
+        }
+      } else -> {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+      }
+    }
   }
 }
